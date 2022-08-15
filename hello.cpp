bed572c4af03b38c2357b8ac1cd1d5ed74c585fb
Christopher Hernandez <cerberean661@gmail.com>
2022-08-15 12:37:45 -0700

    first save

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+int main()
+
+/*
+ * hello.cpp
+ * Prints out my name,
+ * home, and my favorite
+ * hobby.
+ * by Chris
+ */
+
+{
+    // Prints my name
+    printf("Hello World! My name is Chris!\n");
+    // Prints my hometown
+    printf("My hometown is Delano.\n");
+    // Prints my hobby
+    printf("My hobby is playing survival games.");
+}
